169fd2a8a1b25fe3e73d39c8da761c2525a43bc9
ADI NUGROHO <alexiusnicholas@rocketmail.com>
2023-03-20 21:49:18 +0700

    Post Method Added

--- index.js
+++ index.js
@@ -9,4 +9,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let filteredResult = JSON.parse(fs.readFileSync("./data/expense.json"));
+console.log(filteredResult);
+
+app.get("/", (req, res) => {
+  res.status(200).send(filteredResult);
+});
+
+app.get("/:id", (req, res) => {
+  const result = filteredResult.filter((item) => req.params.id == item.id);
+  //   console.log(req.params.id);
+  res.status(200).send(result);
+});
+
+app.post("/", (req, res) => {
+  let incrementID = filteredResult[filteredResult.length - 1].id + 1;
+  filteredResult.push({ id: incrementID, ...req.body });
+  fs.writeFileSync("./data/expense.json", JSON.stringify(filteredResult));
+  res.status(200).send("Data Added Success");
+});
+
 app.listen(PORT, () => console.log("Server running :", PORT));
